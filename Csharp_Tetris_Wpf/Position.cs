b53077a1798ed5232f240dac0834bb4e7e71274b
John Ryan Andal <john.andal@umtc.com.ph>
2024-01-15 16:35:51 +0800

    Initial Commit

---
+++ Csharp_Tetris_Wpf/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Tetris_Wpf
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+
+    }
+}
